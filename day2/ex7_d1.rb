b4fd82603eecdc337046abefb8008ff8c4a1a904
Dmitriy Pinchuk <d.pinchuk02@gmail.com>
2023-04-15 19:48:55 +0300

    day 2: exercise 7

---
+++ day2/ex7_d1.rb
@@ -0,0 +1,39 @@
+# Print a string
+puts "Mary had a little lamb."
+# Print a string with interpolated string "snow"
+puts "Its fleece was white as #{'snow'}."
+# Print a string
+puts "And everywhere that Mary went."
+# Print dot "." character 10 times
+puts "." * 10  # what'd that do?
+
+# Assign character "C" to variable end1
+end1 = "C"
+# Assign character "h" to variable end2
+end2 = "h"
+# Assign character "e" to variable end3
+end3 = "e"
+# Assign character "e" to variable end4
+end4 = "e"
+# Assign character "s" to variable end5
+end5 = "s"
+# Assign character "e" to variable end6
+end6 = "e"
+# Assign character "B" to variable end7
+end7 = "B"
+# Assign character "u" to variable end8
+end8 = "u"
+# Assign character "r" to variable end9
+end9 = "r"
+# Assign character "g" to variable end10
+end10 = "g"
+# Assign character "e" to variable end11
+end11 = "e"
+# Assign character "r" to variable end12
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# Concat end1, end2, end3, end4, end and end6 variables and print without line break
+print end1 + end2 + end3 + end4 + end5 + end6
+# Concat end7, end8, end9, end10, end11 and end12 variables and print with line break
+puts end7 + end8 + end9 + end10 + end11 + end12
