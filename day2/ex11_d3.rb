e278f0c18abda75e859be57dd83eade3fb4b7eec
Dmitriy Pinchuk <d.pinchuk02@gmail.com>
2023-04-15 20:56:56 +0300

    day 2: exercise 11

---
+++ day2/ex11_d3.rb
@@ -0,0 +1,10 @@
+# Study drill, ex. 11, point 3
+# Write another form
+
+print "What's your name? "
+name = gets.chomp
+
+print "How do you call your pet? "
+pet_name = gets.chomp
+
+puts "OK, your name is #{name}, and your pet is #{pet_name}."
